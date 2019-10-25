84f64ac922903c83cde7fd25c0fedc3d7ccb1882
Nathan Dunn <ndunn@me.com>
2019-10-25 15:09:44 -0700

    added another example

--- src/demo/index.js
+++ src/demo/index.js
@@ -14,10 +14,7 @@ ratExamples();
 mouseExamples();
 flyExamples();
 
-function flyExamples(){
-  // 2L:132412..230018
-// http://localhost:8080/apollo/vcf/remotefly/Phenotypic%20Variants/2L:132412..230018.json?includeGenotypes=false&ignoreCache=true
-// http://localhost:8080/apollo/track/remotefly/All%20Genes/2L:132412..230018.json?includeGenotypes=false&ignoreCache=true
+function createExample(isoformDataFunction,variantDataFunction,divId){
   let configGlobal1 = {
     "locale": "global",
     "chromosome": '2L',
@@ -29,13 +26,13 @@ function flyExamples(){
         "id": 12,
         "genome":"Fly",
         "type": "isoform_variant",
-        "isoformFunction": fakeAgrDataService.GetFakeFlyGeneExample1(),
+        "isoformFunction": isoformDataFunction,
         "isoform_url": [
           "https://agr-apollo.berkeleybop.io/apollo/track/",
           "/All%20Genes/",
           ".json"
         ],
-        "variantFunction": fakeAgrDataService.GetFakeFlyVariantExample1(),
+        "variantFunction": variantDataFunction,
         "variant_url": [
           "https://agr-apollo.berkeleybop.io/apollo/vcf/",
           "/Phenotypic%20Variants/",
@@ -46,192 +43,36 @@ function flyExamples(){
     ]
   };
 
-  new GenomeFeatureViewer(configGlobal1, "#viewerFlyExample1", 900, 500);
+  new GenomeFeatureViewer(configGlobal1, `#${divId}`, 900, 500);
+}
+
+function flyExamples(){
+  // 2L:132412..230018
+// http://localhost:8080/apollo/vcf/remotefly/Phenotypic%20Variants/2L:132412..230018.json?includeGenotypes=false&ignoreCache=true
+// http://localhost:8080/apollo/track/remotefly/All%20Genes/2L:132412..230018.json?includeGenotypes=false&ignoreCache=true
+  createExample(fakeAgrDataService.GetFakeFlyGeneExample1(),fakeAgrDataService.GetFakeFlyVariantExample1(),"viewerFlyExample1");
+  createExample(fakeAgrDataService.GetFakeFlyGeneExample2(),fakeAgrDataService.GetFakeFlyVariantExample2(),"viewerFlyExample2");
 }
 
 function ratExamples(){
   // http://localhost:8080/apollo/vcf/remotemouse/Phenotypic%20Variants/6:113619452..113636198.json?includeGenotypes=false&ignoreCache=true
   // http://localhost:8080/apollo/track/remotemouse/All%20Genes/6:113619452..113636198.json?includeGenotypes=false&ignoreCache=true  let configGlobal1 = {
-  let configGlobal1 = {
-    "locale": "global",
-    "chromosome": '2L',
-    "start": 132412,
-    "end": 230018,
-    "showVariantLabel":true ,
-    "tracks": [
-      {
-        "id": 12,
-        "genome":"Fly",
-        "type": "isoform_variant",
-        "isoformFunction": fakeAgrDataService.GetFakeRatGeneExample1(),
-        "isoform_url": [
-          "https://agr-apollo.berkeleybop.io/apollo/track/",
-          "/All%20Genes/",
-          ".json"
-        ],
-        "variantFunction": fakeAgrDataService.GetFakeRatVariantExample1(),
-        "variant_url": [
-          "https://agr-apollo.berkeleybop.io/apollo/vcf/",
-          "/Phenotypic%20Variants/",
-          ".json"
-        ],
-
-      },
-    ]
-  };
-
-  new GenomeFeatureViewer(configGlobal1, "#viewerRatExample1", 900, 500);
+  createExample(fakeAgrDataService.GetFakeRatGeneExample1(),fakeAgrDataService.GetFakeRatVariantExample1(),"viewerRatExample1");
 }
 function mouseExamples(){
   // http://localhost:8080/apollo/vcf/remotemouse/Phenotypic%20Variants/6:113619452..113636198.json?includeGenotypes=false&ignoreCache=true
   // http://localhost:8080/apollo/track/remotemouse/All%20Genes/6:113619452..113636198.json?includeGenotypes=false&ignoreCache=true  let configGlobal1 = {
-  let configGlobal1 = {
-    "locale": "global",
-    "chromosome": '2L',
-    "start": 132412,
-    "end": 230018,
-    "showVariantLabel":true ,
-    "tracks": [
-      {
-        "id": 12,
-        "genome":"Fly",
-        "type": "isoform_variant",
-        "isoformFunction": fakeAgrDataService.GetFakeMouseGeneExample1(),
-        "isoform_url": [
-          "https://agr-apollo.berkeleybop.io/apollo/track/",
-          "/All%20Genes/",
-          ".json"
-        ],
-        "variantFunction": fakeAgrDataService.GetFakeMouseVariantExample1(),
-        "variant_url": [
-          "https://agr-apollo.berkeleybop.io/apollo/vcf/",
-          "/Phenotypic%20Variants/",
-          ".json"
-        ],
-
-      },
-    ]
-  };
-
-  new GenomeFeatureViewer(configGlobal1, "#viewerMouseExample1", 900, 500);
-
-
+  createExample(fakeAgrDataService.GetFakeMouseGeneExample1(),fakeAgrDataService.GetFakeMouseVariantExample1(),"viewerMouseExample1");
 }
 
 function fishExamples(){
-  let configGlobal5 = {
-    "locale": "global",
-    "chromosome": 'V',
-    "start": 20824,
-    "end": 43697,
-    "showVariantLabel":true ,
-    "tracks": [
-      {
-        "id": 12,
-        "genome":"Mus musculus",
-        "type": "isoform_variant",
-        "isoformFunction": fakeAgrDataService.GetFakeFishGeneDataLbx2(),
-        "isoform_url": [
-          "https://agr-apollo.berkeleybop.io/apollo/track/",
-          "/All%20Genes/",
-          ".json"
-        ],
-        "variantFunction": fakeAgrDataService.GetFakeFishVariantDataLbx2(),
-        "variant_url": [
-          "https://agr-apollo.berkeleybop.io/apollo/vcf/",
-          "/Phenotypic%20Variants/",
-          ".json"
-        ],
-
-      },
-    ]
-  };
-
-  new GenomeFeatureViewer(configGlobal5, "#viewerFishLbx2", 900, 500);
-
-  let configGlobal6 = {
-    "locale": "global",
-    "chromosome": 'V',
-    "start": 20824,
-    "end": 43697,
-    "showVariantLabel":false,
-    "tracks": [
-      {
-        "id": 12,
-        "genome":"Mus musculus",
-        "type": "isoform_variant",
-        "isoformFunction": fakeAgrDataService.GetFakeFishGeneDataMyl7(),
-        "isoform_url": [
-          "https://agr-apollo.berkeleybop.io/apollo/track/",
-          "/All%20Genes/",
-          ".json"
-        ],
-        "variantFunction": fakeAgrDataService.GetFakeFishVariantDataMyl7(),
-        "variant_url": [
-          "https://agr-apollo.berkeleybop.io/apollo/vcf/",
-          "/Phenotypic%20Variants/",
-          ".json"
-        ],
-
-      },
-    ]
-  };
-
-  new GenomeFeatureViewer(configGlobal6, "#viewerFishMyl7", 900, 500);
+  createExample(fakeAgrDataService.GetFakeFishGeneDataLbx2(),fakeAgrDataService.GetFakeFishVariantDataLbx2(),"viewerFishLbx2");
+  createExample(fakeAgrDataService.GetFakeFishGeneDataMyl7(),fakeAgrDataService.GetFakeFishVariantDataMyl7(),"viewerFishMyl7");
 }
 
 
 function wormExamples(){
-  let configGlobal4 = {
-    "locale": "global",
-    "chromosome": 'V',
-    "start": 20824,
-    "end": 43697,
-    "showVariantLabel":false,
-    "transcriptTypes":  [
-      'mRNA', 'ncRNA', 'piRNA'
-      , 'lincRNA'
-      , 'miRNA'
-      , 'pre_miRNA'
-      , 'snoRNA'
-      , 'lnc_RNA'
-      , 'tRNA'
-      , 'snRNA'
-      , 'rRNA'
-      , 'ARS'
-      , 'antisense_RNA'
-
-
-      , 'C_gene_segment'
-      , 'V_gene_segment'
-      , 'pseudogene_attribute'
-      ,'snoRNA_gene'
-    ],
-    "variantTypes": ['point_mutation','MNV','Deletion','Insertion','Delins'],
-    "tracks": [
-      {
-        "id": 12,
-        "genome":"Mus musculus",
-        "type": "isoform_variant",
-        "isoformFunction": fakeAgrDataService.GetFakeWormGeneDataEgl8(),
-        "isoform_url": [
-          "https://agr-apollo.berkeleybop.io/apollo/track/",
-          "/All%20Genes/",
-          ".json"
-        ],
-        "variantFunction": fakeAgrDataService.GetFakeWormVariantDataEgl8(),
-        "variant_url": [
-          "https://agr-apollo.berkeleybop.io/apollo/vcf/",
-          "/Phenotypic%20Variants/",
-          ".json"
-        ],
-
-      },
-    ]
-  };
-
-  new GenomeFeatureViewer(configGlobal4, "#viewerWormEgl8", 900, 400);
-
+  createExample(fakeAgrDataService.GetFakeWormGeneDataEgl8(),fakeAgrDataService.GetFakeWormVariantDataEgl8(),"viewerWormEgl8");
 }
 
 
